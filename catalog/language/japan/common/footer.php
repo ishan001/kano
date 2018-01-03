929527b99ce731d99b3537dc5dca21346c4b197b
Ishan Jayamanne <ishanjmails@gmail.com>
2018-01-03 19:38:56 +0000

    update footer powered by link

--- catalog/language/japan/common/footer.php
+++ catalog/language/japan/common/footer.php
@@ -14,4 +14,4 @@ $_['text_account']      = 'アカウント情報';
 $_['text_order']        = '注文履歴';
 $_['text_wishlist']     = 'ウイッシュリスト';
 $_['text_newsletter']   = 'ニュースレター';
-$_['text_powered']      = 'Powered By <a href="http://www.kanotrading.com">kanotrading.com</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Powered By <a href="http://www.kanotrading.co.jp">kanotrading.co.jp</a><br /> %s &copy; %s';
